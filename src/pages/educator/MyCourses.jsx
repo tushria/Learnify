5ddb3cdebd5597489ad589c1404e089adbf9bdb2
Sakshi Saini <sakshi.ts2002@gmail.com>
2025-07-13 15:43:14 +0530

    updated the educator section

--- src/pages/educator/MyCourses.jsx
+++ src/pages/educator/MyCourses.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import {useContext, useEffect, useState} from 'react'
+import Loading from '../../components/student/Loading'
+import { AppContext } from '../../context/AppContext'
 
 const MyCourses = () => {
-  return (
+  const{currency, allCourses} = useContext (AppContext)
+  const [courses, setCourses] = useState(null)
+  const fetchEducatorCourses = async ()=>{
+    setCourses(allCourses)
+  }
+  useEffect(()=>{
+    fetchEducatorCourses()
+  },[])
+  return courses ? (
     <div>
       <h1>My courses</h1>
     </div>
-  )
+  ) : <Loading/>
 }
 
 export default MyCourses
